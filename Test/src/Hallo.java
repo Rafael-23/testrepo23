cabe9c0fc924fe4058c0bd479ebb05876bc96079
Rafael Ehren <Rafael.Ehren@uni-duesseldorf.de>
2014-04-19 14:26:16 +0200

    and another commit

--- Test/src/Hallo.java
+++ Test/src/Hallo.java
@@ -4,6 +4,7 @@
 public class Hallo {
     public static void main(String[] args){
         System.out.println("Hallo, Github!");
-        System.out.println("Heyho!");
+        System.out.println("Hey!");
+        System.out.println("Ho!");
     }
 }
